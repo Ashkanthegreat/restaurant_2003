dd8264c8668039c7e3852673c88fc4401c76fcaa
Ashkan Abbasi <airashkan@yahoo.com>
2020-03-18 13:28:38 -0600

    Finish iteration 2

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,1 +1,17 @@
+class Restaurant
+  attr_reader :opening_time, :name, :dishes
 
+  def initialize(opening_time, name)
+    @opening_time = opening_time
+    @name = name
+    @dishes = []
+  end
+
+  def closing_time(hours_open)
+    (@opening_time.chomp(':00').to_i + hours_open).to_s.concat(':00')
+  end
+
+  def add_dish(dish)
+    @dishes << dish 
+  end
+end
